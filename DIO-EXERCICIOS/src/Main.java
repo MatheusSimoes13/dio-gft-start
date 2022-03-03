dfcf9f07155f21d995a36861118d0c0d69858176
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-03-02 22:23:29 -0300

    Exercicio Map

--- DIO-EXERCICIOS/src/Main.java
+++ DIO-EXERCICIOS/src/Main.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import comparators.ComparatorAnoNome;
@@ -27,6 +32,7 @@ public class Main {
 	System.out.println("2 - Perguntas sobre crime");
 	System.out.println("3 - Cores do arco-íris");
 	System.out.println("4 - Linguagem Favorita");
+	System.out.println("5 - Populações");
 	op = sc.nextInt();
 	
 	switch(op){
@@ -241,7 +247,121 @@ public class Main {
 					System.out.println(linguagem);
 				}
 				break;
-		
+				///////////////////////// FIM DO EXERCÍCIO 4////////////////////
+				///////////////////////// MAPS /////////////////////
+				
+				
+			case 5:
+				
+				Map<String,Integer> populacoes = new HashMap();
+				
+				populacoes.put("PE",9616621);
+				populacoes.put("AL",3351543);
+				populacoes.put("CE",9187103);
+				populacoes.put("RN",3534265);
+				
+				System.out.println(populacoes.toString());
+				
+				System.out.println("Substituir população do RN");
+				
+				populacoes.put("RN",3534165);
+				System.out.println(populacoes.get("RN"));
+				
+				System.out.println("Se não tiver PB, adicione PB,4039277");
+				
+				if(!populacoes.containsKey("PB")) {
+					populacoes.put("PB",4039277);
+				}
+				
+				System.out.println("População PE");
+				
+				System.out.println(populacoes.get("PE"));
+				
+				System.out.println("Estados e Populações");
+				
+				Map<String,Integer> populacoesOrdem = new LinkedHashMap(populacoes);
+				
+				for(Entry<String, Integer> populacao : populacoesOrdem.entrySet()) {
+					System.out.println(populacao.getKey() + " - " + populacao.getValue());
+				}
+				
+				System.out.println("Ordem alfabética");
+				
+				Map<String,Integer> populacoesAlfabetica = new TreeMap(populacoes);
+				
+				for(Entry<String, Integer> populacao : populacoesAlfabetica.entrySet()) {
+					System.out.println(populacao.getKey() + " - " + populacao.getValue());
+				}
+				
+				System.out.println("Menor população");
+				
+				Entry<String,Integer> min = null;
+				for(Entry<String,Integer> populacao : populacoes.entrySet()) {
+					if(min == null || min.getValue() > populacao.getValue()) {
+						min = populacao;
+					}
+				}
+				
+				System.out.println(min.getKey() + " - " + min.getValue());
+				
+				System.out.println("Maior população");
+				
+				Entry<String,Integer> max = null;
+				for(Entry<String,Integer> populacao : populacoes.entrySet()) {
+					if(max == null || max.getValue() < populacao.getValue()) {
+						max = populacao;
+					}
+				}
+				
+				System.out.println(max.getKey() + " - " + max.getValue());
+				
+				System.out.println("Soma");
+				
+				int soma;
+				
+				soma = min.getValue() + max.getValue();
+				System.out.println(soma);
+				
+				System.out.println("Média das populações");
+				
+				int somaTodos = 0;
+				double media = 0;
+				
+				for(Entry<String,Integer> populacao : populacoes.entrySet()) {
+					somaTodos += populacao.getValue();
+				}
+				
+				media = somaTodos/populacoes.size();
+				
+				System.out.println(media);
+				
+				System.out.println("Remover estados com população menor que 4.000.000");
+				
+				Iterator<Integer> itMap = populacoes.values().iterator();
+				
+				while(itMap.hasNext()) {
+					if(itMap.next() < 4000000) {
+					itMap.remove();
+						System.out.println("Removeu");
+					}
+				}
+				
+				
+				for(Entry<String, Integer> populacao : populacoesOrdem.entrySet()) {
+					System.out.println(populacao.getKey() + " - " + populacao.getValue());
+				}
+				
+				System.out.println("Apagar tudo");
+				
+				populacoes.clear();
+				
+				System.out.println("Está vazio?");
+				
+				System.out.println(populacoes.isEmpty());
+				
+				
+				break;
+				
 	}
 	}
 
